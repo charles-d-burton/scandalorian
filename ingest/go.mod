25d70647ea0d2e28de28ceda80b554a0bbd66ef4
charles-d-burton <charles.burton@mindflash.com>
2019-12-15 17:10:30 -0700

    feat: ingest api complete

---
+++ ingest/go.mod
@@ -0,0 +1,18 @@
+module github.com/charles-d-burton/kanscan/ingest/
+
+go 1.13
+
+require (
+	github.com/gin-gonic/gin v1.5.0
+	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/google/uuid v1.1.1
+	github.com/json-iterator/go v1.1.8 // indirect
+	github.com/leodido/go-urn v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.11 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/spf13/viper v1.6.1
+	golang.org/x/sys v0.0.0-20191210023423-ac6580df4449 // indirect
+	gopkg.in/go-playground/validator.v9 v9.30.2 // indirect
+	gopkg.in/yaml.v2 v2.2.7 // indirect
+)
---
+++ ingest/main.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"github.com/spf13/viper"
+)
+
+//ScanRequest object instructing system on how to scan.
+type ScanRequest struct {
+	ID      string `json:"id,omitempty"`
+	Address string `json:"address,omitempty"`
+	Host    string `json:"host,omitempty"`
+}
+
+//Scan structure to send to message queue for scanning
+type Scan struct {
+	IP      string      `json:"ip"`
+	IsLast  bool        `json:"is_last"`
+	Request ScanRequest `json:"scan_request"`
+}
+
+func main() {
+	v := viper.New()
+	v.SetEnvPrefix("ingest")
+	v.AutomaticEnv()
+
+	router := gin.Default()
+	router.POST("/scan", handlePost)
+	router.Run(":9090")
+}
+
+func handlePost(c *gin.Context) {
+	var scanRequest ScanRequest
+	if err := c.ShouldBindJSON(&scanRequest); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if scanRequest.Address == "" && scanRequest.Host == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "host or address undefined"})
+		return
+	} else if scanRequest.Address != "" && scanRequest.Host != "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "host and address defined"})
+		return
+	}
+	if scanRequest.Address != "" {
+		parts := strings.Split(scanRequest.Address, "/") //Check for CIDR notation
+		if len(parts) < 2 {
+			scanRequest.Address = scanRequest.Address + "/32"
+		} else if len(parts) > 2 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "bad address"})
+			return
+		}
+		cidrval, err := strconv.Atoi(parts[1])
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		if cidrval < 24 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "subnet out of range"})
+			return
+		}
+		ip, _, err := net.ParseCIDR(scanRequest.Address)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		if ip.IsLoopback() {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "cannot scan loopback"})
+			return
+		}
+	}
+	scanRequest.ID = uuid.New().String()
+	if err := enQueueRequest(&scanRequest); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+}
+
+func enQueueRequest(scanreq *ScanRequest) error {
+	var scans []Scan
+	if scanreq.Host != "" {
+		addr, err := net.LookupIP(scanreq.Host)
+		if err != nil {
+			return errors.New("Unknown Host")
+		} else {
+			fmt.Println("IP address: ", addr)
+			for idx, address := range addr {
+				var scan Scan
+				scan.IsLast = false
+				scan.IP = address.String()
+				scan.Request = *scanreq
+				if idx == len(addr)-1 {
+					scan.IsLast = true
+				}
+				scans = append(scans, scan)
+			}
+		}
+	} else {
+		addrs, err := Hosts(scanreq.Address)
+		if err != nil {
+			return err
+		}
+		var scan Scan
+		for idx, addr := range addrs {
+			scan.IP = addr
+			scan.Request = *scanreq
+			if idx == len(addr)-1 {
+				scan.IsLast = true
+			}
+			scans = append(scans, scan)
+		}
+
+	}
+	for _, scan := range scans {
+		log.Println(scan)
+	}
+	return nil
+}
+
+//Hosts split cidr into individual IP addresses
+func Hosts(cidr string) ([]string, error) {
+	ip, ipnet, err := net.ParseCIDR(cidr)
+	if err != nil {
+		return nil, err
+	}
+
+	var ips []string
+	for ip := ip.Mask(ipnet.Mask); ipnet.Contains(ip); inc(ip) {
+		ips = append(ips, ip.String())
+	}
+	// remove network address and broadcast address
+	if len(ips) == 1 {
+		return ips, nil
+	}
+	return ips[1 : len(ips)-1], nil
+}
+
+func inc(ip net.IP) {
+	for j := len(ip) - 1; j >= 0; j-- {
+		ip[j]++
+		if ip[j] > 0 {
+			break
+		}
+	}
+}

4c0e8e2d64c441274535ca67f26788f8e0f203e3
charles-d-burton <charles.burton@mindflash.com>
2019-12-28 16:47:00 -0700

    added scan def

--- shared/shared.go
+++ shared/shared.go
@@ -8,3 +8,17 @@ const (
 	Discovery ScanType = iota
 	Nmap
 )
+
+//ScanRequest object instructing system on how to scan.
+type ScanRequest struct {
+	ID      string `json:"id,omitempty"`
+	Address string `json:"address,omitempty"`
+	Host    string `json:"host,omitempty"`
+}
+
+//Scan structure to send to message queue for scanning
+type Scan struct {
+	IP      string      `json:"ip"`
+	Type    ScanType    `json:"type"`
+	Request ScanRequest `json:"scan_request"`
+}

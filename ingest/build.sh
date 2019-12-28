ca0e5fe76a42a8295de9f3a7bba165d6e2bbc093
charles-d-burton <charles.burton@mindflash.com>
2019-12-27 19:17:38 -0700

    feat: multi-arch build

--- ingest/build.sh
+++ ingest/build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-cd /tmp
 export PCAPV=1.9.1
 wget http://www.tcpdump.org/release/libpcap-$PCAPV.tar.gz
 tar xvf libpcap-$PCAPV.tar.gz
@@ -9,5 +7,25 @@ export CC=arm-linux-gnueabi-gcc
 ./configure --host=arm-linux --with-pcap=linux
 make
 
-env CC=arm-linux-gnueabi-gcc CGO_ENABLED=1 GOOS=linux GOARCH=arm CGO_LDFLAGS="-L/tmp/libpcap-$PCAPV" go build -o ingest-arm .
+cd ..
+env CC=arm-linux-gnueabi-gcc CGO_ENABLED=1 GOOS=linux GOARCH=arm CGO_LDFLAGS="-L./libpcap-$PCAPV" go build -o ingest-arm .
+file ingest-arm
 
+rm -rf libpcap-$PCAPV
+wget http://www.tcpdump.org/release/libpcap-$PCAPV.tar.gz
+tar xvf libpcap-$PCAPV.tar.gz
+cd libpcap-$PCAPV
+export CC=aarch64-linux-gnu-gcc
+export CFLAGS='-Os'
+./configure --host=aarch64-unknown-linux-gnu --with-pcap=linux
+cat config.log
+make
+
+
+cd ..
+env CC=aarch64-linux-gnu-gcc CGO_ENABLED=1 GOOS=linux GOARCH=arm64 CGO_LDFLAGS="-L./libpcap-$PCAPV" go build -o ingest-arm64 .
+file ingest-arm64
+
+env CC=gcc CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -o ingest-amd64 .
+
+file ingest-amd64

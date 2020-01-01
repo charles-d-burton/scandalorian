7e88705538b6117c0e12b3f2c5f2f6a3dad1556e
charles-d-burton <charles.burton@mindflash.com>
2020-01-01 11:56:40 -0700

    feat: faster/better ip conversion

--- ingest/main.go
+++ ingest/main.go
@@ -172,27 +172,59 @@ func enQueueRequest(scanreq *shared.ScanRequest) error {
 
 //Hosts split cidr into individual IP addresses
 func Hosts(cidr string) ([]string, error) {
-	ip, ipnet, err := net.ParseCIDR(cidr)
-	if err != nil {
-		return nil, err
+	var ip uint32 // ip address
+
+	var ipS uint32 // Start IP address range
+	var ipE uint32 // End IP address range
+	cidrParts := strings.Split(cidr, "/")
+
+	ip = iPv4ToUint32(cidrParts[0])
+	bits, _ := strconv.ParseUint(cidrParts[1], 10, 32)
+
+	if ipS == 0 || ipS > ip {
+		ipS = ip
 	}
 
-	var ips []string
-	for ip := ip.Mask(ipnet.Mask); ipnet.Contains(ip); inc(ip) {
-		ips = append(ips, ip.String())
+	ip = ip | (0xFFFFFFFF >> bits)
+
+	if ipE < ip {
+		ipE = ip
 	}
-	// remove network address and broadcast address
-	if len(ips) == 1 {
-		return ips, nil
+	//ipStart := uInt32ToIPv4(ipS)
+	log.Infof("Start of range: %d\n", lastOctet(ipS))
+	//ipEnd := uInt32ToIPv4(ipE)
+	log.Infof("End of Range: %d\n", lastOctet(ipE))
+	ips := make([]string, 0)
+	for w := lastOctet(ipS); w <= lastOctet(ipE); w++ {
+		ips = append(ips, uInt32ToIPv4(w))
 	}
-	return ips[1 : len(ips)-1], nil
+	return ips, nil
 }
 
-func inc(ip net.IP) {
-	for j := len(ip) - 1; j >= 0; j-- {
-		ip[j]++
-		if ip[j] > 0 {
-			break
-		}
+//Convert IPv4 to uint32
+func iPv4ToUint32(iPv4 string) uint32 {
+
+	ipOctets := [4]uint64{}
+
+	for i, v := range strings.SplitN(iPv4, ".", 4) {
+		ipOctets[i], _ = strconv.ParseUint(v, 10, 32)
 	}
+
+	result := (ipOctets[0] << 24) | (ipOctets[1] << 16) | (ipOctets[2] << 8) | ipOctets[3]
+
+	return uint32(result)
+}
+
+//Convert uint32 to IP
+func uInt32ToIPv4(iPuInt32 uint32) (iP string) {
+	iP = fmt.Sprintf("%d.%d.%d.%d",
+		iPuInt32>>24,
+		(iPuInt32&0x00FFFFFF)>>16,
+		(iPuInt32&0x0000FFFF)>>8,
+		iPuInt32&0x000000FF)
+	return iP
+}
+
+func lastOctet(iPuInt32 uint32) uint32 {
+	return iPuInt32 & 0x000000FF
 }

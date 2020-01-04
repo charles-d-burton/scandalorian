281aaa59914274e481b86ae0aa68005c0c18a522
charles-d-burton <charles.burton@mindflash.com>
2020-01-03 19:25:07 -0700

    fix: moved ports to scan object, added pps

--- shared/shared.go
+++ shared/shared.go
@@ -12,15 +12,17 @@ const (
 //TODO: These need rethought a bit
 //ScanRequest object instructing system on how to scan.
 type ScanRequest struct {
-	ID      string   `json:"id,omitempty"`
-	Address string   `json:"address,omitempty"`
-	Host    string   `json:"host,omitempty"`
-	Ports   []string `json:"ports,omitempty"`
+	ID      string `json:"id,omitempty"`
+	Address string `json:"address,omitempty"`
+	Host    string `json:"host,omitempty"`
+	PPS     int    `json:"pps,omitempty"`
 }
 
 //Scan structure to send to message queue for scanning
 type Scan struct {
 	IP      string      `json:"ip"`
 	Type    ScanType    `json:"type"`
+	Ports   []string    `json:"ports,omitempty"`
+	Result  string      `json:"result,omitempty"`
 	Request ScanRequest `json:"scan_request"`
 }

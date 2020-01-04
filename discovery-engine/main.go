73d88a6d273b2d1fe8566ad71f9f5d9457724592
charles-d-burton <charles.burton@mindflash.com>
2020-01-03 21:28:04 -0700

    fix: more rate limit debugging

--- discovery-engine/main.go
+++ discovery-engine/main.go
@@ -337,7 +337,7 @@ func (scw *ScanWork) scan(pWorker *PcapWorker) error {
 	limited := false
 	log.Debugf("rate limit set to: %d", scw.Scan.Request.PPS)
 	if scw.Scan.Request.PPS > 0 {
-		limiter = rate.NewLimiter(rate.Limit(scw.Scan.Request.PPS), 10)
+		limiter = rate.NewLimiter(rate.Every(time.Duration(scw.Scan.Request.PPS)*time.Second), 1)
 		limited = true
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
@@ -345,8 +345,11 @@ func (scw *ScanWork) scan(pWorker *PcapWorker) error {
 	for {
 		// Use the limiter if the desired packet per second is defined
 		if limited {
-			log.Debug("limited by rate limiter on %v", tcp.DstPort)
-			limiter.Wait(ctx) //Wait for the rate limit
+			log.Debugf("rate limited on port: %v", tcp.DstPort)
+			err := limiter.Wait(ctx) //Wait for the rate limit
+			if err != nil {
+				log.Debug(err)
+			}
 		}
 		// Send one packet per loop iteration until we've sent packets
 		// to all of ports [1, 65535].
